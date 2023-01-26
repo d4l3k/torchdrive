a7c6503fa8f2aa10812d6848370c491822ac208d
Tristan Rice <rice@fn.lc>
2023-01-26 14:03:19 -0800

    fix CI build (#1)

--- torchdrive/test_amp.py
+++ torchdrive/test_amp.py
@@ -1,9 +1,11 @@
 import unittest
 
 from torchdrive.amp import autocast
+from torchdrive.testing import skipIfNoCUDA
 
 
 class TestAMP(unittest.TestCase):
+    @skipIfNoCUDA()
     def test_autocast(self) -> None:
         with autocast():
             pass
