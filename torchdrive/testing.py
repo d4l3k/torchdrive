6ab3b64a817326e4cbfe35895643bd6c6c7fa6e8
Tristan Rice <rice@fn.lc>
2023-01-27 00:07:46 -0800

    testing: remove xformers helper

--- torchdrive/testing.py
+++ torchdrive/testing.py
@@ -1,5 +1,3 @@
-# pyre-ignore-all-errors[21]: missing optional imports
-
 import random
 import unittest
 from contextlib import contextmanager
@@ -15,15 +13,6 @@ def skipIfNoCUDA(reason: str = "test requires CUDA") -> Generator[None, None, No
     if not torch.cuda.is_available():
         raise unittest.SkipTest(reason)
     yield
-
-
-def xformers_available() -> bool:
-    try:
-        import xformers  # noqa: F401
-
-        return True
-    except ImportError:
-        return False
 
 
 def manual_seed(seed: int) -> None:
