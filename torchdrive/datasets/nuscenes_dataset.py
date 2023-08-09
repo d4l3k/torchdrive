nuscenes: added rotation transformation matrixes and verified car and camera positions are correct

torchdrive/datasets/nuscenes_dataset.py
@@ -1,5 +1,4 @@
 import os
-import traceback
 from bisect import bisect_left
 from datetime import timedelta
 from enum import Enum
@@ -9,7 +8,7 @@ import torch
 import torchvision.transforms as transforms
 from nuscenes.nuscenes import NuScenes
 from PIL import Image
-from pytorch3d.transforms import quaternion_to_matrix, RotateAxisAngle
+from pytorch3d.transforms import quaternion_to_matrix
 from torch.utils.data import ConcatDataset, DataLoader, Dataset
 
 Tensor = torch.Tensor
@@ -142,11 +141,21 @@ class SceneDataset(Dataset):
         K = torch.eye(4)
         K[:3, :3] = camera_intrinsic
 
-        # Get world to car translation matrix cam_T (still needs to be transformed relative to identify matrix)
+        # Get world to car translation matrix cam_T
         pose_token = sample_data["ego_pose_token"]
         pose = self.nusc.get("ego_pose", pose_token)
         cam_T = torch.eye(4)
-        cam_T[:3, 3] = torch.tensor(pose["translation"])
+        translation = torch.tensor(pose["translation"])
+        cam_T[:3, 3] = translation
+        cam_T = cam_T.inverse()  # convert to car_to_world
+
+        # apply rotation matrix
+        rotation_mat = torch.eye(4)
+        quat = torch.tensor(pose["rotation"])
+        rotation = quaternion_to_matrix(quat)
+        rotation_mat[:3, :3] = rotation
+
+        cam_T = rotation_mat.inverse().matmul(cam_T)
 
         timestamp = sample_data["timestamp"]
 
@@ -154,7 +163,7 @@ class SceneDataset(Dataset):
 
         img = Image.open(img_path)
         # Resize to (640, 480) [H, W]
-        img = img.resize((640, 480))
+        # img = img.resize((640, 480))
         transform = transforms.Compose(
             [
                 transforms.PILToTensor(),
@@ -165,12 +174,7 @@ class SceneDataset(Dataset):
         img = transform(img)
 
         # Get car to camera local translation matrix T
-        # Due to coordinate system differences, we need to rotate the camera 90 degrees counterclockwise around the z-axis.
         rotation = quaternion_to_matrix(torch.tensor(calibrated_sensor["rotation"]))
-        z_rotation = (
-            RotateAxisAngle(90, axis="Z", device="cpu").get_matrix()[0, :3, :3].T
-        )  # transpose is required to convert pytorch3d rotations (clockwise.T = counterclockwise)
-        rotation = z_rotation @ rotation
         translation = calibrated_sensor["translation"]
         T = torch.eye(4)
         T[:3, :3] = rotation
@@ -208,7 +212,7 @@ class SceneDataset(Dataset):
         frame_Ts = [torch.eye(4)]
         for i in range(1, len(frame_dicts)):
             # Get the relative frame_T for this frame by comparing the current cam_T to the previous cam_T
-            frame_T = torch.linalg.inv(cam_Ts[i - 1]) @ cam_Ts[i]
+            frame_T = cam_Ts[i - 1].inverse().matmul(cam_Ts[i])
             # TODO use torch.solve instead
             # frame_T = torch.linalg.solve(cam_Ts[i], cam_Ts[i - 1])[0]
             frame_Ts.append(frame_T)
@@ -352,17 +356,7 @@ class NuscenesDataset(Dataset):
 
     def __getitem__(self, idx: int) -> Optional[Batch]:
         """Aggregates the data from each camera type into an element of a Batch."""
-        try:
-            batch = self._getitem(idx)
-        except IndexError as e:
-            traceback.print_exc()
-            print(f"IndexError at {idx}")
-            return None
-        except Exception as e:
-            traceback.print_exc()
-            print(f"Exception at {idx}")
-            return None
-        return batch
+        return self._getitem(idx)
 
 
 if __name__ == "__main__":

torchdrive/transforms/batch.py
@@ -2,6 +2,8 @@ from abc import ABC, abstractmethod
 from dataclasses import replace
 from typing import Tuple
 
+import torch
+
 from torchdrive.data import Batch
 from torchdrive.transforms.mat import random_translation, random_z_rotation
 
@@ -50,10 +52,44 @@ class NormalizeCarPosition(BatchTransform):
 
     def __call__(self, batch: Batch) -> Batch:
         start_T = batch.cam_T[:, self.start_frame]
-        inv_start_T = start_T.unsqueeze(1).pinverse()
-        cam_T = inv_start_T.matmul(batch.cam_T)
+        inv_start_T = start_T.unsqueeze(1)
+        cam_T = inv_start_T.matmul(batch.cam_T.inverse()).inverse()
         long_cam_T, long_cam_T_mask, long_cam_T_lengths = batch.long_cam_T
         long_cam_T = inv_start_T.matmul(long_cam_T)
+
+        return replace(
+            batch,
+            cam_T=cam_T,
+            long_cam_T=(long_cam_T, long_cam_T_mask, long_cam_T_lengths),
+        )
+
+
+class CenterCar(BatchTransform):
+    """
+    Centers the car XYZ position relative to the target frame. Preserves
+    rotations.
+    """
+
+    def __init__(self, start_frame: int) -> None:
+        self.start_frame = start_frame
+
+    def __call__(self, batch: Batch) -> Batch:
+        # calculate the xyz for the target frame
+        BS = batch.batch_size()
+        start_T = batch.cam_T[:, self.start_frame].inverse()
+        point = torch.tensor((0, 0, 0, 1.0))
+        coord = start_T.matmul(point)
+        coord = coord / coord[:, 3:4]  # normalize w
+
+        # create inverse position transform
+        transform = torch.eye(4).repeat(BS, 1, 1)
+        transform[:, :3, 3] = -coord[:, :3]
+        transform = transform.unsqueeze(1)  # [BS, 1, 4, 4]
+
+        # apply transformation matrix to car position matrices
+        cam_T = transform.matmul(batch.cam_T.inverse()).inverse()
+        long_cam_T, long_cam_T_mask, long_cam_T_lengths = batch.long_cam_T
+        long_cam_T = transform.matmul(long_cam_T)
 
         return replace(
             batch,

torchdrive/transforms/test_batch.py
@@ -6,6 +6,7 @@ import torch
 from torchdrive.data import Batch, dummy_batch
 from torchdrive.transforms.batch import (
     BatchTransform,
+    CenterCar,
     Compose,
     Identity,
     NormalizeCarPosition,
@@ -50,7 +51,7 @@ class TestBatchTransforms(unittest.TestCase):
         out = transform(batch)
 
         # origin shouldn't change
-        zero = torch.tensor((0, 0, 0, 1.0)).expand(2, -1).unsqueeze(-1)
+        zero = torch.tensor((0, 0, 0.1, 1.0)).expand(2, -1).unsqueeze(-1)
         torch.testing.assert_close(out.cam_T[:, 1].matmul(zero), zero)
 
     def test_random_translation(self) -> None:
@@ -64,3 +65,14 @@ class TestBatchTransforms(unittest.TestCase):
         # origin shouldn't change
         zero = torch.tensor((0, 0, 0, 1.0)).expand(2, -1).unsqueeze(-1)
         torch.testing.assert_close(out.cam_T[:, 1].matmul(zero), zero)
+
+    def test_center_car(self) -> None:
+        transform = CenterCar(start_frame=1)
+        batch = dummy_batch()
+        out = transform(batch)
+
+        # center should be zero point
+        zero = torch.tensor((0, 0, 0, 1.0)).expand(2, -1).unsqueeze(-1)
+        new_pos = out.cam_T[:, 1].matmul(zero)
+        new_pos /= new_pos[:, 3:4]  # normalize w
+        torch.testing.assert_close(new_pos, zero)