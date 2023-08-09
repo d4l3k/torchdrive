transforms/batch: fixed long_cam_T normalization

torchdrive/tasks/test_bev.py
@@ -20,17 +20,17 @@ class DummyBEVTask(BEVTask):
         ctx.add_scalar("test", bev.shape[-1])
 
         # check that start position is at zero
-        cam_T = batch.cam_T[:, ctx.start_frame]
+        car_to_world = batch.car_to_world(ctx.start_frame)
         zero = (
-            cam_T.new_tensor([0.0, 0.0, 0.0, 1.0])
-            .expand(cam_T.size(0), -1)
+            car_to_world.new_tensor([0.0, 0.0, 0.0, 1.0])
+            .expand(car_to_world.size(0), -1)
             .unsqueeze(-1)
         )
         long_cam_T, mask, lengths = batch.long_cam_T
         assert isinstance(long_cam_T, torch.Tensor)
-        long_cam_T = long_cam_T[:, ctx.start_frame]
-        torch.testing.assert_allclose(cam_T.matmul(zero), zero)
-        torch.testing.assert_allclose(long_cam_T.matmul(zero), zero)
+        long_car_to_world = long_cam_T[:, ctx.start_frame].pinverse()
+        torch.testing.assert_allclose(car_to_world.matmul(zero), zero)
+        torch.testing.assert_allclose(long_car_to_world.matmul(zero), zero)
 
         return {
             "foo": torch.tensor(5.0),