755828cceecce1504ed87feb080ae47064949455
Tristan Rice <rice@fn.lc>
2023-11-01 00:19:38 -0700

    torchworld/vis: added grid_3d_occupancy

--- torchworld/test_vis.py
+++ torchworld/test_vis.py
@@ -5,8 +5,8 @@ import pythreejs
 import torch
 
 from torchworld.structures.cameras import PerspectiveCameras
-from torchworld.structures.grid import GridImage
-from torchworld.vis import add_camera, add_grid_image
+from torchworld.structures.grid import Grid3d, GridImage
+from torchworld.vis import add_camera, add_grid_3d_occupancy, add_grid_image
 
 
 class TestVis(unittest.TestCase):
@@ -15,7 +15,7 @@ class TestVis(unittest.TestCase):
         out = add_camera(scene, PerspectiveCameras())
         self.assertIsInstance(out, pythreejs.AxesHelper)
 
-    def test_grid_iamge(self) -> None:
+    def test_grid_image(self) -> None:
         scene = pythreejs.Scene()
         img = GridImage(
             data=torch.rand(1, 3, 10, 20),
@@ -23,4 +23,13 @@ class TestVis(unittest.TestCase):
             time=torch.rand(1),
         )
         out = add_grid_image(scene, img)
-        self.assertIsInstance(out, pythreejs.AxesHelper)
+        self.assertIsInstance(out, pythreejs.Mesh)
+
+    def test_grid_3d_occupancy(self) -> None:
+        scene = pythreejs.Scene()
+        grid = Grid3d.from_volume(
+            data=torch.zeros(1, 1, 3, 4, 5),
+            voxel_size=1 / 3,
+        )
+        out = add_grid_3d_occupancy(scene, grid)
+        self.assertIsInstance(out, pythreejs.Mesh)
--- torchworld/vis.py
+++ torchworld/vis.py
@@ -1,8 +1,10 @@
 import numpy as np
 import pythreejs
+import torch
+from matplotlib import cm
 
 from torchworld.structures.cameras import CamerasBase
-from torchworld.structures.grid import GridImage
+from torchworld.structures.grid import Grid3d, GridImage
 from torchworld.transforms.img import normalize_img_cuda
 
 
@@ -29,6 +31,15 @@ def add_camera(scene: pythreejs.Scene, camera: CamerasBase) -> pythreejs.Object3
 
 
 def add_grid_image(scene: pythreejs.Scene, image: GridImage) -> pythreejs.Object3D:
+    """
+    add_grid_image creates a geometry object for the provided RGB image and
+    camera and adds it to the specified scene.
+
+    Arguments
+    ---------
+    scene: scene to use
+    image: image to render
+    """
     cam = add_camera(scene, image.camera)
     dist = 1.0
     plane = pythreejs.BufferGeometry(
@@ -117,5 +128,145 @@ def add_grid_image(scene: pythreejs.Scene, image: GridImage) -> pythreejs.Object
     mesh.matrix = tuple(T.contiguous().view(-1).tolist())
 
     # cam.add([mesh])
+    scene.add([mesh])
+    return mesh
+
+
+def add_grid_3d_occupancy(
+    scene: pythreejs.Scene,
+    grid: Grid3d,
+    p: float = 0.5,
+    palette: str = "magma",
+    eps: float = 1e-8,
+) -> pythreejs.Object3D:
+    """
+    Creates a geometry object for the provided voxel grid and camera and adds it
+    to the specified scene. This must be an occupancy grid with a single channel
+    and values between 0 and 1.
+
+    Arguments
+    ---------
+    scene: scene to use
+    grid: occupancy grid to render
+    p: probability threshold for voxels to render
+    palette: the matplotlib color palette to use
+    eps: a small value to avoid numerical issues
+    """
+    BS, ch, Z, Y, X = grid.data.shape
+    N = X * Y * Z
+    device = grid.data.device
+    grid_shape = grid.grid_shape()
+
+    if ch != 1:
+        raise TypeError("must have single channel")
+
+    cmap = cm.get_cmap(palette)
+    num_colors = 1000
+    colors = torch.tensor(
+        [cmap(i / num_colors)[:3] for i in range(num_colors)], device=device
+    )
+
+    data = grid.data.permute(0, 1, 4, 3, 2)
+    grid_shape = grid_shape[::-1]
+
+    channels = torch.meshgrid(
+        *(torch.arange(-1, 1 - eps, 2 / dim, device=device) for dim in grid_shape),
+        indexing="ij",
+    )
+    grid_points = torch.stack(channels, dim=-1).unsqueeze(0)
+
+    matching = data[0] >= p
+
+    offsets = grid_points[matching]
+    values = data[0][matching]
+    colors = torch.index_select(colors, dim=0, index=(values * (num_colors - 1)).int())
+    # offsets = grid.local_to_world.transform_points(offsets)
+    N = offsets.size(0)
+
+    vertices = np.array(
+        [
+            [-0.5, -0.5, -0.5],
+            [0.5, -0.5, -0.5],
+            [0.5, 0.5, -0.5],
+            [-0.5, 0.5, -0.5],
+            [-0.5, -0.5, 0.5],
+            [0.5, -0.5, 0.5],
+            [0.5, 0.5, 0.5],
+            [-0.5, 0.5, 0.5],
+        ],
+        dtype=np.float32,
+    )
+    vertices /= grid_shape
+    vertices *= 2
+    faces = np.array(
+        [
+            [0, 4, 3],
+            [3, 4, 7],
+            [1, 2, 6],
+            [1, 6, 5],
+            [0, 1, 5],
+            [0, 5, 4],
+            [2, 3, 7],
+            [2, 7, 6],
+            [0, 2, 1],
+            [0, 3, 2],
+            [4, 5, 7],
+            [5, 6, 7],
+        ],
+        dtype=np.uint32,
+    )
+
+    colors = pythreejs.InstancedBufferAttribute(
+        array=colors.numpy(), meshPerAttribute=3
+    )
+    offsets = pythreejs.InstancedBufferAttribute(
+        array=offsets.numpy(), meshPerAttribute=3
+    )
+
+    instancedGeometry = pythreejs.InstancedBufferGeometry(
+        maxInstancedCount=N * 3,
+        attributes={
+            "position": pythreejs.BufferAttribute(array=vertices),
+            "index": pythreejs.BufferAttribute(array=faces.ravel()),
+            "offset": offsets,
+            "color": colors,
+        },
+    )
+
+    material = pythreejs.ShaderMaterial(
+        vertexShader="""
+            precision highp float;
+            attribute vec3 offset;
+            attribute vec4 rotation;
+            varying vec3 vPosition;
+            varying vec4 vColor;
+            void main(){
+
+                vPosition = position + 2.0 * cross( rotation.xyz, cross( rotation.xyz, position ) + rotation.w * position );
+                vPosition += offset;
+
+                vColor = vec4( color, 1.0);
+                gl_Position = projectionMatrix * modelViewMatrix * vec4( vPosition, 1.0 );
+            }
+            """,
+        fragmentShader="""
+            precision highp float;
+            varying vec4 vColor;
+            void main() {
+                gl_FragColor = vec4( vColor );
+            }
+            """,
+        vertexColors="VertexColors",
+        transparent=True,
+    )
+
+    # mat = pythreejs.MeshBasicMaterial()
+    mesh = pythreejs.Mesh(instancedGeometry, material)
+
+    T = grid.local_to_world
+    T = T.get_matrix()
+    mesh.matrixAutoUpdate = False
+    mesh.matrix = tuple(T.contiguous().view(-1).tolist())
+
     scene.add([mesh])
     return mesh
